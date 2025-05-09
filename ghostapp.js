08f6a7399aa31109bde45589ce15433939ce8cfa
JotaJota0 <devkarim60@gmail.com>
2025-05-10 00:48:22 +0100

    Update ghostapp.js

--- ghostapp.js
+++ ghostapp.js
@@ -1,1 +1,5 @@
-J4u2kf4fRLFdcT/zMY3S/7/TdkuQeNuamyeFBSZMziAbAy9phCikvaqkwf3gqFrRiTt+Db79S+9jk5YZ8UgJt30Mcy3SBMJm8upQiCBuRpA=
+{
+    "user_ghost": "ghostteam",
+    "pass_ghost": "free",
+    "statusapp": "online"
+}
